8ade29a440fd9841146d87a3d9691870416e660a
Mark J Morales Reynoso <amanjibtw@gmail.com>
2025-09-03 15:25:00 +0000

    added code to match assignment

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
- 
+int main() { 
+ int height = 61; 
+ int weight = 130;
+
+double kg = weight * 0.453592;
+double m = height * 0.0254;
+double BMI = kg / (m * m);
+cout << "The BMI for this person is: " << BMI << endl;
 
 
 }
